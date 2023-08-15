905f750c8df6efcf8e42215c881c5d897549170c
Luke Griggs <lukegriggs@Lukes-MacBook-Air.local>
2023-08-15 17:03:44 -0500

    set up the image component. Sourced image from unsplash and also fetched weather data

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
-import Image from 'next/image'
-import axios from 'axios'
-import {useState} from 'react'
-import {BsSearch} from 'react-icons/bs'
-export default function Home() {
+'use client'
 
-  const[city, setCity] = useState('');
+import Image from 'next/image';
+import axios from 'axios';
+import {useState} from 'react';
+import {BsSearch} from 'react-icons/bs';
+
+export default function Home() {
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
   
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
   
+  const fetchWeather = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    axios.get(url).then((response) => {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+    setCity('')
+    setLoading(false)
+  }
   
   return (
     <div>
-      
+      {/*overlay*/}
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'/>
+      {/* Background Image*/}
+     <Image src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1675&q=80' 
+            layout= 'fill'
+            alt = '/'
+            className='object-cover'
+            />
+  
+{/*Search*/}
+
     </div>
-  )
+  );
 }
