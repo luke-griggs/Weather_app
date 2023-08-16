225f90de894ac8c01da61a2d17fc8c0cd6f869e0
Luke Griggs <lukegriggs@Lukes-MacBook-Air.local>
2023-08-16 17:59:13 -0500

    set up search UI as well as styled the search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,66 @@
-'use client'
+"use client";
 
-import Image from 'next/image';
-import axios from 'axios';
-import {useState} from 'react';
-import {BsSearch} from 'react-icons/bs';
+import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import Weather from '../components/Weather';
+
 
 export default function Home() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
-  
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
-  
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
   const fetchWeather = (e) => {
-    e.preventDefault()
-    setLoading(true)
+    e.preventDefault();
+    setLoading(true);
     axios.get(url).then((response) => {
-      setWeather(response.data)
-      console.log(response.data)
-    })
-    setCity('')
-    setLoading(false)
-  }
-  
+      setWeather(response.data);
+      console.log(response.data);
+    });
+    setCity("");
+    setLoading(false);
+  };
+
   return (
     <div>
       {/*overlay*/}
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'/>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
       {/* Background Image*/}
-     <Image src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1675&q=80' 
-            layout= 'fill'
-            alt = '/'
-            className='object-cover'
+      <Image
+        src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1675&q=80"
+        layout="fill"
+        alt="/"
+        className="object-cover"
+      />
+
+      {/*Search*/}
+
+      <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10">
+        <form
+          onSubmit={fetchWeather}
+          className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl"
+        >
+          <div>
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              className="bg-transparent border-none text-white focus:outline-none text-2xl:"
+              type="text"
+              placeholder="Search City"
             />
-  
-{/*Search*/}
+          </div>
+          <button onClick={fetchWeather}>
+            <BsSearch size={20} />
+          </button>
+        </form>
+      </div>
+    
+      {/* Weather */}
+
+      {weather.main && <Weather />}
 
     </div>
   );
